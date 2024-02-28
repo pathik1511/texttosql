617fb697be7089ece3c8d1477cb248c29566b792
pathik1511 <patelpathik1110@gmail.com>
2024-02-27 22:04:31 -0500

    SQL Databse Created

---
+++ readme.md
@@ -0,0 +1,9 @@
+Project Flow
+
+Prompt --> LLM --> Gemini Pro --> Query --> SQL Databse --> Response
+
+
+Implementation
+
+1) SQLlite --> Insert some records --> Python Programming
+2) LLM Application --> Gemini Pro --> SQl Database
--- sql.py
+++ sql.py
@@ -1,3 +1,30 @@
 import sqlite3
 ## conection 
-connection=sqlite3.connect("student.db")
+connection=sqlite3.connect("student1.db")
+
+##creating a cursor resposible for retrieving and creating table
+cursor = connection.cursor()
+table_info = """
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25), MARKS INT)
+
+"""
+cursor.execute(table_info)
+
+
+##CMD For inserting records
+cursor.execute('''INSERT INTO STUDENT VALUES ('Krish', 'Data Science', 'A',90)''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Darius', 'Data Science', 'B',100)''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Sudhanshu', 'Devops', 'C',86)''') 
+cursor.execute('''INSERT INTO STUDENT VALUES ('Vikash', 'Data Science', 'C',50)''') 
+
+# Display data inserted 
+print("Data Inserted in the table: ") 
+data=cursor.execute('''SELECT * FROM STUDENT''') 
+for row in data: 
+    print(row) 
+  
+# Commit your changes in the database     
+connection.commit()
+# Closing the connection 
+connection.close()
